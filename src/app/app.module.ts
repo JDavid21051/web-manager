437e7382f7a36eb5674c8ffbc6874255413a1238
JDavid21051 <david.pelaezcumbe@gmail.com>
2023-08-23 15:44:35 -0500

    --@nebular/theme installed --@nebular/auth installed --@nebular/moment installed --@nebular/date-fns installed --@nebular/eva-icons installed --eva-icons installed --moment installed --date-fns installed --layout added --nebular styles added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  NbThemeModule,
+  NbLayoutModule,
+  NbSidebarModule,
+  NbSidebarService,
+  NbCardModule,
+  NbIconModule, NbButtonModule
+} from '@nebular/theme';
+import {NbEvaIconsModule} from '@nebular/eva-icons';
+
+
+export const NebularModules = [
+  NbThemeModule.forRoot({name: 'corporate'}),
+  NbLayoutModule,
+  NbSidebarModule,
+  NbEvaIconsModule,
+  NbIconModule,
+  NbCardModule,
+  NbButtonModule
+];
 
 @NgModule({
   declarations: [
@@ -10,9 +31,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ...NebularModules
   ],
-  providers: [],
+  providers: [NbSidebarService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
